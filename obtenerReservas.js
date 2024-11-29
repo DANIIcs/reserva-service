8a1b0b530efb19c586bad7ab7217893a9813a6d3
Daniel <humbertodanielcaballeros@gmail.com>
2024-11-29 11:46:14 -0500

    obtenerReserva

--- obtenerReservas.js
+++ obtenerReservas.js
@@ -1,21 +1,37 @@
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-const RESERVA_TABLE = process.env.TABLE_NAME_RESERVA;
 
 exports.handler = async () => {
+    console.log('Obteniendo todas las reservas');
+
     try {
-        const params = { TableName: RESERVA_TABLE };
+        // Validar variables de entorno
+        if (!process.env.TABLE_NAME_RESERVA) {
+            return {
+                statusCode: 500,
+                status: 'Internal Server Error - Variable de entorno TABLE_NAME_RESERVA no configurada',
+            };
+        }
+
+        const tablaReservas = process.env.TABLE_NAME_RESERVA;
+
+        // Escanear todas las reservas en DynamoDB
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+        const params = { TableName: tablaReservas };
+
         const result = await dynamodb.scan(params).promise();
 
+        // Respuesta exitosa
         return {
             statusCode: 200,
-            body: JSON.stringify({ reservas: result.Items }),
+            message: 'Reservas obtenidas exitosamente',
+            reservas: result.Items,
         };
     } catch (error) {
+        console.error(`Error inesperado: ${error.message}`);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Error al obtener las reservas' }),
+            status: 'Internal Server Error - Error al obtener las reservas',
+            error: error.message,
         };
     }
 };
